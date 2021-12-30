f41f2c9bcb4d0b0ceb107cfb6e9d46fe8d7ae9f7
Eleazar <jai690@hotmail.com>
2021-12-30 16:26:40 -0600

    mudar logica de reiniciar juego un nivel arriba

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import TableroCartas from "./TableroCartas";
 import InfoBar from "./InfoBar";
 
 const App = ()=> {
+  const [gameStatus,setGameStatus] = useState('active');
+
+  const checkStatus = (status) => {
+      setGameStatus(status)
+  }
+
   const [gameId, setGameId] = useState(1);
   return (
     <div>
       <InfoBar />
-      <TableroCartas key={gameId} startNewGame={() => setGameId(gameId + 1)}/>
+      <TableroCartas 
+          key={gameId} startNewGame={() => setGameId(gameId + 1)}
+          checkStatus={checkStatus}
+          gameStatus={gameStatus}
+          />
     </div>
   );
 }
--- src/components/TableroCartas.js
+++ src/components/TableroCartas.js
@@ -3,18 +3,12 @@ import MarvelList from "./MarvelList";
 import PlayAgain from "./playAgain";
 
 const TableroCartas = (props)=> {
-    const [gameStatus,setGameStatus] = useState('active');
-
-    const checkStatus = (status) => {
-        setGameStatus(status)
-    }
-
     return (
         <div>
-        {gameStatus !== 'active' ? (
-                <PlayAgain onClick={props.startNewGame} gameStatus={gameStatus} />
+        {props.gameStatus !== 'active' ? (
+                <PlayAgain onClick={props.startNewGame} gameStatus={props.gameStatus} />
             ) : (
-                <MarvelList actualizar={checkStatus} />
+                <MarvelList actualizar={props.checkStatus} />
             )}
         </div>
   );
